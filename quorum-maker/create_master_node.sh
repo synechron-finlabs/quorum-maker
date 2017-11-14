90eed73401b204fa03bebb7716cd680bc93596e2
Rakhi.Bondre <rakhi.bondre@synechron.com>
2017-11-14 19:42:12 +0530

    added genesis init only for master node and init script to start master node

--- quorum-maker/create_master_node.sh
+++ quorum-maker/create_master_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
  function copyConfTemplate(){
      PATTERN="s/#mNode#/${mNode}/g"
      sed $PATTERN lib/template.conf > ${mNode}/node/${mNode}.conf
@@ -7,8 +7,6 @@
      otherNodeUrl=""
      mNode1=${nodes[0]}
 
-    PATTERN="s/#cport#/${cPort}/g"
-    sed -i $PATTERN ${mNode}/node/${mNode}.conf
  }
 
  function generateKeyPair(){
@@ -19,6 +17,7 @@
     constellation-node --generatekeys=${mNode}a
 
     mv ${mNode}*.*  ${mNode}/node/keys/.
+    
  }
 
 
@@ -28,6 +27,9 @@ function createInitNodeScript(){
     START_CMD="start_$mNode.sh"
 
     PATTERN="s/#start_cmd#/${START_CMD}/g"
+    sed -i $PATTERN ${mNode}/init.sh
+
+    PATTERN="s/#mNode#/${mNode}/g"
     sed -i $PATTERN ${mNode}/init.sh
 
     chmod +x ${mNode}/init.sh
@@ -41,6 +43,7 @@ function copyStartTemplate(){
     cat lib/start_template.sh >> ${mNode}/node/start_${mNode}.sh
     PATTERN="s/#mNode#/${mNode}/g"
     sed -i $PATTERN ${mNode}/node/start_${mNode}.sh
+    chmod +x ${mNode}/node/start_${mNode}.sh
 }
 
 function generateEnode(){
@@ -65,59 +68,22 @@ function generateEnode(){
     rm nodekey
 }
 
-
 function createAccount(){
     mAccountAddress="$(geth --datadir datadir --password lib/passwords.txt account new)"
     re="\{([^}]+)\}"
     if [[ $mAccountAddress =~ $re ]];
     then
         mAccountAddress="0x"${BASH_REMATCH[1]};
-
-        mMakers[$mNodeIndex]=$mAccountAddress
-        let "mNodeIndex++"
     fi
     mv datadir/keystore/* ${mNode}/node/qdata/keystore/${mNode}key
     rm -rf datadir
 
+    PATTERN="s/#mNodeAddress#/${mAccountAddress}/g"
+    cat lib/genesis_template.json > ${mNode}/node/genesis.json
+    sed -i $PATTERN ${mNode}/node/genesis.json
+    cd ${mNode}/node
+    geth --datadir qdata init genesis.json
 }
-
-# function writeGenesisConfig(){
-#     git clone https://github.com/davebryson/quorum-genesis.git
-#     cd quorum-genesis
-#     echo "{" > ${mNode}/quorum-config.json
-#     echo "    \"threshold\":$voterIndex," >> ${mNode}/quorum-config.json
-#     echo "    \"makers\": [" >> ${mNode}/quorum-config.json
-
-#     j=0
-#     comma=","
-#     while [ $j -lt $mNodeIndex ]
-#     do
-#         let k="(( $j + 1 ))"
-#         if [ $k -eq $mNodeIndex ];
-#         then comma=""
-#         fi
-
-#         echo     \"${mMakers[$j]}\"$comma >> ${mNode}/quorum-config.json
-#         let "j++"
-#     done
-#     echo "    ]," >> ${mNode}/quorum-config.json
-#     cp quorum-config.json ./${mNode}/.
-
-# }
-
-# function generateGenesis(){
-#     writeGenesisConfig
-#     #cp quorum-config.json ./${mNode}/.
-#     cd ${mNode}
-#     npm init
-#     npm install -g
-#     quorum-genesis
-#     cd ..
-#     rm -f ${mNode}/quorum-config.json
-#     rm -rf quorum-genesis
-    
-#     mv ${mNode}/quorum-genesis.json ${mNode}/genesis.json
-# }
 
 function main(){    
     read -p $'\e[1;32mPlease enter master node name: \e[0m' mNode 
@@ -129,8 +95,8 @@ function main(){
     generateKeyPair
     createInitNodeScript
     copyStartTemplate
-    createAccount
-    #generateGenesis
     generateEnode
+    createAccount
+    
 }
 main
--- quorum-maker/lib/start_template.sh
+++ quorum-maker/lib/start_template.sh
@@ -1,11 +1,17 @@
+echo 'CURRENT_IP='$pCurrentIp >  ../nodeSetup.conf
+echo 'RPC_PORT='$rPort >>  ../nodeSetup.conf
+echo 'WHISPER_PORT='$wPort >>  ../nodeSetup.conf
+echo 'CONSTELLATION_PORT='$cPort >>  ../nodeSetup.conf
+echo 'MASTER_JAVA_PORT='$mjPort >>  ../nodeSetup.conf
+
 GLOBAL_ARGS="--raft --networkid $NETID --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
 
 echo "[*] Starting Constellation node" > qdata/logs/constellation_#mNode#.log
 
 cp qdata/#mNode#.conf .
 
-PATTERN="s/#CURRENT_NODE_IP#/${CURRENT_NODE_IP}/g"
-PATTERN2="s/#C_PORT#/${C_PORT}/g"
+PATTERN="s/#CURRENT_IP#/${pCurrentIp}/g"
+PATTERN2="s/#C_PORT#/${cPort}/g"
 
 sed -i "$PATTERN" #mNode#.conf
 sed -i "$PATTERN2" #mNode#.conf
@@ -14,7 +20,7 @@ constellation-node #mNode#.conf 2>> qdata/logs/constellation_#mNode#.log &
 sleep 1
 
 echo "[*] Starting #mNode# node" >> qdata/logs/#mNode#.log
-echo "[*] geth --verbosity 6 --datadir qdata" $GLOBAL_ARGS" --raftport 50401 --rpcport "$R_PORT "--port "$W_PORT "#blockMakerPattern# #voterPattern# --minblocktime 2 --maxblocktime 5 --nat extip:"$CURRENT_NODE_IP>> qdata/logs/#mNode#.log
+echo "[*] geth --verbosity 6 --datadir qdata" $GLOBAL_ARGS" --raftport 50401 --rpcport "$rPort "--port "$W_PORT "--nat extip:"$pCurrentIp>> qdata/logs/#mNode#.log
 
-PRIVATE_CONFIG=#mNode#.conf geth --verbosity 6 --datadir qdata $GLOBAL_ARGS --raftport 50401 --rpcport $R_PORT --port $W_PORT #blockMakerPattern# #voterPattern# --minblocktime 2 --maxblocktime 5 --nat extip:$CURRENT_NODE_IP 2>>qdata/logs/#mNode#.log 
+PRIVATE_CONFIG=#mNode#.conf geth --verbosity 6 --datadir qdata $GLOBAL_ARGS --raftport 50401 --rpcport $R_PORT --port $W_PORT --nat extip:$pCurrentIp 2>>qdata/logs/#mNode#.log 
 
