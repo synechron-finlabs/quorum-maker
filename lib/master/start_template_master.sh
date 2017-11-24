01cb8179a202d3f111b6a25acd433b4f2615db7c
RakhiB <Rakhi.Bondre@synechron.com>
2017-11-24 16:55:15 +0530

    cleanUp master branch

---
+++ lib/master/start_template_master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -u
+set -e
+NETID=#network_Id_value#
+
+GLOBAL_ARGS="--raft --nodiscover --networkid $NETID --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum"
+
+echo "[*] Starting Constellation node" > qdata/logs/constellation_#mNode#.log
+
+constellation-node #mNode#.conf 2>> qdata/logs/constellation_#mNode#.log &
+sleep 1
+
+echo "[*] Starting #mNode# node" >> qdata/logs/#mNode#.log
+echo "[*] geth --verbosity 6 --datadir qdata" $GLOBAL_ARGS" --raftport 50400 --rpcport "$R_PORT "--port "$W_PORT "--nat extip:"$CURRENT_NODE_IP>> qdata/logs/#mNode#.log
+
+PRIVATE_CONFIG=#mNode#.conf geth --verbosity 6 --datadir qdata $GLOBAL_ARGS --raftport 50400 --rpcport $R_PORT --port $W_PORT --nat extip:$CURRENT_NODE_IP 2>>qdata/logs/#mNode#.log 
+
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#Menu system for launching appropriate scripts based on user choice
+flagmain=true
+while  ("${flagmain}" = true) 
+do
+	echo -e 'Please select an option:\n1) Create Master Node\n2) Join Network\n3) Remove Node\n4) Setup Development/Test Network\n5) Exit'
+	printf 'option: '
+	read option
+	case $option in
+		1)
+			./lib/create_master_node.sh &
+			PID1=$!
+			wait $PID1
+			;;
+		2)
+			./lib/join_network.sh &
+			PID1=$!
+			wait $PID1
+			;;
+		3)
+			./lib/remove_node.sh &
+			PID1=$!
+			wait $PID1
+			;; 
+		4)
+			./lib/setup_network.sh &
+			PID1=$!
+			wait $PID1
+			;;
+		5)
+			flagmain=false
+			;;
+		*)
+			echo "Please enter a valid option"
+			;;
+	esac
+done
