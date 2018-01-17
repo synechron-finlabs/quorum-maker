ef229432fcd519c2bb3c78cacee8e4b94bd888f7
abhaya renuse <abhaya.renuse@synechron.com>
2018-01-17 11:06:08 +0530

    file naming conventions are done

--- Docker_compose/lib/setup.sh
+++ Docker_compose/lib/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-
-read -p $'\e[1;36mWould you like to use this with raft_docker-compose support? [y/N] \e[0m' yn
-
-case $yn in
-    [Yy]* )
-	lib/raft_docker_compose.sh
-	;;
-    * )
-	lib/setup_multibox.sh
-	;;
-esac
+echo ""
+echo "Creating Network...."
+lib/raft_docker_compose.sh
 
--- Docker_compose/lib/start_template_common.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -u
-set -e
-NETID=87234
-
-
