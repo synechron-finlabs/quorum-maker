e38aef8c821806f8e8892a24ca2ed02e1d16ecdf
Dhyan <dhyan@Dhyans-Mac.local>
2018-06-13 19:21:38 -0700

    Fix to export ports on Mac OS

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 #Menu system for launching appropriate scripts based on user choice
 source qm.variables
 
+#Fix to automatically export ports on Mac OS. 
+os=$(uname)
+if [ "$os" = "Darwin" ]; then
+	touch .qm_export_ports
+fi
+
 docker run -it --rm -v $(pwd)/$line:/${PWD##*/} -w /${PWD##*/} $dockerImage lib/menu.sh
 
 if [ -f .nodename ]; then
