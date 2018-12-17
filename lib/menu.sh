392323971eb2271316c56fdf65601a45ee98a383
Dhyan Raj <dhyan.raj@synechron.com>
2018-12-16 23:34:55 -0500

    Added QM banner

--- lib/menu.sh
+++ lib/menu.sh
@@ -4,6 +4,23 @@
 source qm.variables
 source lib/common.sh
 
+function banner() {
+	printf $CYAN'   ______'$RED'   __      __\n' 
+	printf $CYAN'  / ____ \'$RED' |  \    /  |  \n'
+	printf $CYAN' / /    \ \'$RED'|   \  /   |  \n'
+	printf $CYAN' | |     | '$RED'|    \/    |  \n'
+	printf $CYAN' | |     | '$RED'| |\    /| |  \n'
+	printf $CYAN' | |     |  \'$RED'| \  / | |  \n'
+	printf $CYAN' \ \____/ /\ \'$RED'  \/  | |  \n'
+	printf $CYAN'  \______/  \_\'$RED'     |_|  '
+
+	local __version=$(egrep -Eo "[0-9].*" <<< $dockerImage)
+	    
+	IFS='_'; arrIN=($__version); unset IFS;
+
+	echo -e $GREEN'Version '${arrIN[1]}' Built on Quorum '${arrIN[0]}'\n'
+}
+
 function readParameters() {
     POSITIONAL=()
     while [[ $# -gt 0 ]]
@@ -51,6 +68,7 @@ function readParameters() {
 
 function main() {
 
+	banner
 	readParameters $@
 
 	if [ -z "$NON_INTERACTIVE" ]; then
