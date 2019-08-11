fabcc730346b5bfaff266451d3c9a45bfd6e5053
Dhyan Raj <dhyanraj@gmail.com>
2019-08-11 15:25:34 -0400

    Kubernetes support

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ if [ "$os" = "Darwin" ]; then
 	touch .qm_export_ports
 fi
 
-docker run -it --rm -v $(pwd)/$line:/${PWD##*/} -w /${PWD##*/} $dockerImage lib/menu.sh $@
+lib/menu.sh $@
 
 if [ -f .nodename ]; then
 	nodename=$(cat .nodename)
