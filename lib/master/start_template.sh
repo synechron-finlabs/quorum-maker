f1932f2861c726ece6f40947ef6ee5ddca40cfc9
Dhyan Raj <dhyanraj@gmail.com>
2019-08-11 14:31:06 -0400

    changes for Kubernetes suppot

--- lib/master/start_template.sh
+++ lib/master/start_template.sh
@@ -3,76 +3,33 @@
 source qm.variables
 source node/common.sh
 
-function readParameters() {
-    
-    POSITIONAL=()
-    while [[ $# -gt 0 ]]
-    do
-        key="$1"
+function startNode() {
 
-        case $key in
-            -d|--detached)
-            detached="true"
-            shift # past argument
-            shift # past value
-            ;;          
-            *)    # unknown option
-            POSITIONAL+=("$1") # save it in an array for later
-            shift # past argument
-            ;;
-        esac
-    done
-    set -- "${POSITIONAL[@]}" # restore positional parameters
-
+    CURRENT_NODE_IP=$CURRENT_IP \
+        R_PORT=$RPC_PORT \
+        W_PORT=$WHISPER_PORT \
+        C_PORT=$CONSTELLATION_PORT \
+        RA_PORT=$RAFT_PORT \
+        NODE_MANAGER_PORT=$THIS_NODEMANAGER_PORT \
+        WS_PORT=$WS_PORT \
+        NETID=$NETWORK_ID \
+        NODE_NAME=$NODENAME \
+        ./start_${NODENAME}.sh
 }
 
-# docker command to create a network
-function startNode(){
-    docker kill $NODENAME 2> /dev/null && docker rm $NODENAME 2> /dev/null
+function main() {
 
-    docker run $DOCKER_FLAG --rm --name $NODENAME \
-            -v $(pwd):/home  -w /home/node  \
-            -v $(pwd)/node/contracts:/root/quorum-maker/contracts \
-            -p $RPC_PORT:$RPC_PORT \
-            -p $WHISPER_PORT:$WHISPER_PORT \
-            -p $WHISPER_PORT:$WHISPER_PORT/udp \
-            -p $CONSTELLATION_PORT:$CONSTELLATION_PORT \
-            -p $RAFT_PORT:$RAFT_PORT \
-            -p $THIS_NODEMANAGER_PORT:$THIS_NODEMANAGER_PORT \
-            -p $WS_PORT:$WS_PORT \
-            -e CURRENT_NODE_IP=$CURRENT_IP \
-            -e R_PORT=$RPC_PORT \
-            -e W_PORT=$WHISPER_PORT \
-            -e C_PORT=$CONSTELLATION_PORT \
-            -e RA_PORT=$RAFT_PORT \
-            -e NODE_MANAGER_PORT=$THIS_NODEMANAGER_PORT \
-            -e WS_PORT=$WS_PORT \
-            -e NETID=$NETWORK_ID \
-            -e NODE_NAME=$NODENAME \
-            $dockerImage ./start_${NODENAME}.sh
-}
-
-function main(){
-    
-    docker run -it --rm -v $(pwd):/home  -w /home  \
-              $dockerImage node/pre_start_check.sh $@
+    node/pre_start_check.sh $@
 
     if [ -f setup.conf ]; then
-         source setup.conf
+        source setup.conf
     fi
 
     if [ -z $NETWORK_ID ]; then
         exit
     fi
 
-    readParameters $@
-
-    if [[ -z "$detached" ]]; then 
-	    DOCKER_FLAG="-it"
-    else
-	    DOCKER_FLAG="-d"
-    fi 	
-
+    cd node
     startNode
 }
-main $@
+main
--- lib/slave/start_template.sh
+++ lib/slave/start_template.sh
@@ -3,64 +3,26 @@
 source qm.variables
 source node/common.sh
 
-function readParameters() {
-    
-    POSITIONAL=()
-    while [[ $# -gt 0 ]]
-    do
-        key="$1"
+function startNode() {
 
-        case $key in
-            -d|--detached)
-            detached="true"
-            shift # past argument
-            shift # past value
-            ;;          
-            *)    # unknown option
-            POSITIONAL+=("$1") # save it in an array for later
-            shift # past argument
-            ;;
-        esac
-    done
-    set -- "${POSITIONAL[@]}" # restore positional parameters
-
+    NODENAME=$NODENAME \
+        CURRENT_NODE_IP=$CURRENT_IP \
+        R_PORT=$RPC_PORT \
+        W_PORT=$WHISPER_PORT \
+        C_PORT=$CONSTELLATION_PORT \
+        RA_PORT=$RAFT_PORT \
+        NM_PORT=$THIS_NODEMANAGER_PORT \
+        WS_PORT=$WS_PORT \
+        NETID=$NETWORK_ID \
+        RAFTID=$RAFT_ID \
+        MASTER_IP=$MASTER_IP \
+        MC_PORT=$MASTER_CONSTELLATION_PORT \
+        ./start_$NODENAME.sh
 }
 
-# docker command to join th network 
-function startNode(){
+function main() {
 
-    docker kill $NODENAME 2> /dev/null && docker rm $NODENAME 2> /dev/null
-
-    docker run $DOCKER_FLAG --rm --name $NODENAME \
-           -v $(pwd):/home \
-           -v $(pwd)/node/contracts:/root/quorum-maker/contracts \
-           -w /home/node  \
-           -p $RPC_PORT:$RPC_PORT \
-           -p $WHISPER_PORT:$WHISPER_PORT \
-           -p $WHISPER_PORT:$WHISPER_PORT/udp \
-           -p $CONSTELLATION_PORT:$CONSTELLATION_PORT \
-           -p $RAFT_PORT:$RAFT_PORT \
-           -p $THIS_NODEMANAGER_PORT:$THIS_NODEMANAGER_PORT\
-           -p $WS_PORT:$WS_PORT \
-           -e NODENAME=$NODENAME \
-           -e CURRENT_NODE_IP=$CURRENT_IP \
-           -e R_PORT=$RPC_PORT \
-           -e W_PORT=$WHISPER_PORT \
-           -e C_PORT=$CONSTELLATION_PORT \
-           -e RA_PORT=$RAFT_PORT \
-           -e NM_PORT=$THIS_NODEMANAGER_PORT \
-           -e WS_PORT=$WS_PORT \
-           -e NETID=$NETWORK_ID \
-           -e RAFTID=$RAFT_ID \
-           -e MASTER_IP=$MASTER_IP \
-           -e MC_PORT=$MASTER_CONSTELLATION_PORT \
-           $dockerImage ./start_$NODENAME.sh
-}
-
-function main(){
-    
-    docker run -it --rm -v $(pwd):/home  -w /${PWD##*}/home  \
-              $dockerImage node/pre_start_check.sh
+    node/pre_start_check.sh
 
     source setup.conf
 
@@ -68,14 +30,8 @@ function main(){
         exit
     fi
 
-    readParameters $@
-
-    if [[ -z "$detached" ]]; then     
-	    DOCKER_FLAG="-it"
-    else
-	    DOCKER_FLAG="-d"
-    fi 		
-
+    cd node
+    
     startNode
 }
-main $@
+main
