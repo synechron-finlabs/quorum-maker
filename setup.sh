8ccff8728969e30132121f56571e5373d7d0414c
Dhyan Raj <dhyanraj@gmail.com>
2019-08-11 15:30:59 -0400

    Updated

--- setup.sh
+++ setup.sh
@@ -3,17 +3,6 @@
 #Menu system for launching appropriate scripts based on user choice
 source qm.variables
 
-#Fix to automatically export ports on Mac OS. 
-os=$(uname)
-if [ "$os" = "Darwin" ]; then
-	touch .qm_export_ports
-fi
 
 lib/menu.sh $@
 
-if [ -f .nodename ]; then
-	nodename=$(cat .nodename)
-	rm -f .nodename
-	cd $nodename	
-	./start.sh $@
-fi
